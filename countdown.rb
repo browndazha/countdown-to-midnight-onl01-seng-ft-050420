8b2bcdcb975eeae052c9ec9ec1592b61a49dfb20
Dazha Brown <dajahbrown@hotmail.com>
2020-05-11 20:23:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,7 @@
-number = 1
-while number < 0
-  puts "#{number} SECOND(S)!"
-  x += 1
-end
-#write your code here
-
 def countdown
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
 end
